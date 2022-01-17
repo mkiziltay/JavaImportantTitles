77d12e3d4855377750f1d340c728e9e72bae4dde
Mustafa KIZILTAY <85419797+mkiziltay@users.noreply.github.com>
2022-01-17 20:37:03 +0300

    Update replaceAll(pattern) method.java

--- Main methods/replaceAll(pattern) method.java
+++ Main methods/replaceAll(pattern) method.java
@@ -8,10 +8,10 @@ public class Main
 	    
 	    String str = "Zero,One!Two  *   Three-four~Five.";
 	    
-	    String Fresult = str.replaceAll("\\p{Punct}|\\p{Space}",""); // {Punct}=punctuation => Noktalama işaretleri
+	    String Fresult = str.replaceAll("\\p{Punct}|\\p{Space}",""); // {Punct}=punctuation => Noktalama işaretleri *** {Space} boşluklar
 	    String Sresult = str.replaceAll("[^a-zA-Z0-9]","");          // [^a-zA-Z0-9] = everything except this [a-zA-Z0-9]
 	    
-		System.out.println("First  result is : "+Fresult); // First result is : ZeroOneTwoThreefourFive
+		System.out.println("First  result is : "+Fresult); // First result is  : ZeroOneTwoThreefourFive
 		System.out.println("Second result is : "+Sresult); // Second result is : ZeroOneTwoThreefourFive
 		
 	}
