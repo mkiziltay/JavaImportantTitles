05e8a54c0e8605ac7ebab5e60e9510b337d21f4c
Mustafa KIZILTAY <85419797+mkiziltay@users.noreply.github.com>
2022-01-18 17:26:01 +0300

    Update (str).compareTo(str).java

--- Main methods/(str).compareTo(str).java
+++ Main methods/(str).compareTo(str).java
@@ -4,9 +4,9 @@ public class HelloWorld{
         
         System.out.println("123".compareTo("124") ); //124 is forward and 123 is 1 step backward.
         System.out.println("abc".compareTo("abd") ); //abd is forward and abc is 1 step backward.
-        System.out.println("bbb".compareTo("aa") ); //aa is backward
-        System.out.println( "z".compareTo("x" ) ); //x is 2 step backward in alphabeth
-        System.out.println( "z".compareTo("a" ) ); //a is 25 step backward in alphabeth
+        System.out.println("bbb".compareTo("aa") ); //aa is backward in Eng. alphabeth.
+        System.out.println( "z".compareTo("x" ) ); //x is 2 step backward in alphabeth.
+        System.out.println( "z".compareTo("a" ) ); //a is 25 step backward in alphabeth.
      }
 } 
 /*OUTPUT
