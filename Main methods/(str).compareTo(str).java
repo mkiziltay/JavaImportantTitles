c42ca7e5171c9b9a5bfb51b4d75352d454d5fa95
Mustafa KIZILTAY <85419797+mkiziltay@users.noreply.github.com>
2022-01-18 17:25:23 +0300

    Add files via upload

---
+++ Main methods/(str).compareTo(str).java
@@ -0,0 +1,20 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+        
+        System.out.println("123".compareTo("124") ); //124 is forward and 123 is 1 step backward.
+        System.out.println("abc".compareTo("abd") ); //abd is forward and abc is 1 step backward.
+        System.out.println("bbb".compareTo("aa") ); //aa is backward
+        System.out.println( "z".compareTo("x" ) ); //x is 2 step backward in alphabeth
+        System.out.println( "z".compareTo("a" ) ); //a is 25 step backward in alphabeth
+     }
+} 
+/*OUTPUT
+
+-1
+-1
+1
+2
+25
+
+*/
