7c6bc6d868d87d0d04af1f56f6be81b7a1948601
Mustafa KIZILTAY <85419797+mkiziltay@users.noreply.github.com>
2022-01-23 23:46:37 +0300

    Add files via upload

---
+++ Main methods/String.join method.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+
+public class Main
+{
+    
+	public static void main(String[] args) {
+	    
+	    ArrayList<String> array = new ArrayList<>(Arrays.asList("one", "two", "three","four","five"));
+	    
+	    String seperator = "-";
+	    
+	    String result = String.join(seperator,array);
+	    
+	    // Creating new String from ArrayList with String.join(item,array) method
+	    
+		System.out.println("Array is :" +array.toString()); // ==> Array is :[one, two, three, four, five]
+		System.out.println("with join method result is :" + result); // ==>  result is :one-two-three-four-five
+	}
+}
