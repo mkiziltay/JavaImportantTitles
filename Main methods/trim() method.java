0beb75c0eb747f17f8481cd7726a28e3cf1f9896
Mustafa KIZILTAY <85419797+mkiziltay@users.noreply.github.com>
2022-02-03 22:38:59 +0300

    Add files via upload

---
+++ Main methods/trim() method.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+public class Main
+{
+    
+	public static void main(String[] args) {
+        //initialize string
+        String str = "  \t\n\n\tHello World!\n Welcome to www.tutorialkart.com\n\n\t";
+         
+        //trim method trims(removes) leading and trailing spaces
+        String result = str.trim(); 
+         
+        System.out.print("\""+result+"\"");
+    }
+}
