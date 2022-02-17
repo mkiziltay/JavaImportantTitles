08f46088f64fa3f162fd0c8c7520366bcee2639b
Mustafa KIZILTAY <85419797+mkiziltay@users.noreply.github.com>
2022-02-17 22:02:00 +0300

    Update Collections.swap(params) method.java

--- Main methods/Collections.swap(params) method.java
+++ Main methods/Collections.swap(params) method.java
@@ -1,1 +1,14 @@
+import java.util.*;
 
+public class CollectionsSwapExample1 {  
+    public static void main (String[] args) {  
+        //Create a list with items  
+            ArrayList<String>  list = new ArrayList<String>(Arrays.asList("Third","Second","First","Fourth"));  
+  
+            System.out.println("Original List : \n" +list+"\n");  
+            //Perform swapping : Wrond place 0 and 2 
+            Collections.swap(list, 0, 2);  
+            System.out.println("List after swapping : \n" +list); 
+            
+          }  
+}
