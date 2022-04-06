3368157f96b2883da68f1304120a0a305924dd67
Mustafa KIZILTAY <85419797+mkiziltay@users.noreply.github.com>
2022-04-06 17:35:08 +0300

    Add files via upload

---
+++ Main methods/Arrays.toString() and Arrays.copyOfRange(..).java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+
+public class ArrayClass {
+    
+      public static void main (String[] args) {  
+        String[] array = new String[] {"4","3","2","1","0"};
+        System.out.println(Arrays.toString(array)); // print original array
+        array = Arrays.copyOfRange(array,1,array.length);// remove 0. element
+        System.out.println(Arrays.toString(array)); // print new array
+      }
+    // OUTPUT: from [4, 3, 2, 1, 0] to [3, 2, 1, 0]
+    }
