529d9b426240b3cf634e22a0d7ccd4f1b4c71d85
Mustafa KIZILTAY <85419797+mkiziltay@users.noreply.github.com>
2022-04-06 17:42:10 +0300

    Update Arrays.toString() and Arrays.copyOfRange(..).java

--- Main methods/Arrays.toString() and Arrays.copyOfRange(..).java
+++ Main methods/Arrays.toString() and Arrays.copyOfRange(..).java
@@ -7,7 +7,9 @@ public class ArrayClass {
           
         String[] array = new String[] {"4","3","2","1","0"};
         System.out.println(Arrays.toString(array)); // print original array
-        array = Arrays.copyOfRange(array,1,array.length);// remove 0. element
+          
+          // copy after 0. element
+        array = Arrays.copyOfRange(array,1,array.length); //(0. element will be removed)
         System.out.println(Arrays.toString(array)); // print new array
       }
     // OUTPUT: from [4, 3, 2, 1, 0] to [3, 2, 1, 0]
